943561c2d1a3e13d46f39663e780af13a59a0a38
Matthew Harrigan <harrigan@stanford.edu>
2016-11-30 18:27:17 -0800

    Automatic version determination

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup
+from subprocess import check_output
+import re
+
+tag = check_output(['git', 'describe', '--tags', '--long']).decode()
+ma = re.match(r'v(\d+)\.(\d+)\.(\d+)\-(\d+)\-(\w+)', tag)
+if ma is None:
+    print("The git tag {} does not match the format of v1.y.z-n-abcdef1")
+    exit(1)
+major, minor, patch, dirty, hash = ma.groups()
+if int(dirty) > 0:
+    version = "{major}.{minor}.{patch}.dev{dirty}".format(major=major, minor=minor, patch=patch, dirty=dirty)
+else:
+    version = "{major}.{minor}.{patch}".format(major=major, minor=minor, patch=patch)
+print("Version:", version)
 
 setup(
     name='sphinx_modern_theme',
-    version="1.0.0",
+    version=version,
     url='http://github.com/mpharrigan/sphinx-modern-theme',
     license='MIT',
     author='Matthew Harrigan',
@@ -16,7 +30,6 @@ setup(
             '*.html',
             'static/*.css',
             'static/*.js',
-            ]
-        },
+        ]
+    },
 )
-
