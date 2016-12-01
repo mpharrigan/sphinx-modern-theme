3071264098c6af7b83d5b7636009d21c6d4b3f29
Matthew Harrigan <harrigan@stanford.edu>
2016-11-30 18:57:23 -0800

    Better automatic version determination

--- setup.py
+++ setup.py
@@ -2,16 +2,39 @@ from setuptools import setup
 from subprocess import check_output
 import re
 
-tag = check_output(['git', 'describe', '--tags', '--long']).decode()
-ma = re.match(r'v(\d+)\.(\d+)\.(\d+)\-(\d+)\-(\w+)', tag)
-if ma is None:
-    print("The git tag {} does not match the format of v1.y.z-n-abcdef1")
-    exit(1)
-major, minor, patch, dirty, hash = ma.groups()
-if int(dirty) > 0:
-    version = "{major}.{minor}.{patch}.dev{dirty}".format(major=major, minor=minor, patch=patch, dirty=dirty)
-else:
-    version = "{major}.{minor}.{patch}".format(major=major, minor=minor, patch=patch)
+
+def get_version():
+    """Interrogate git to find our version string.
+
+    We require tags of the form v1.y.z
+
+    Development builds should happen on named branches (pref. `master`).
+    When crafting a release, start a new branch named 1.y
+
+    """
+    tag = check_output(['git', 'describe', '--tags', '--long']).decode()
+    ma = re.match(r'v(\d+)\.(\d+)\.(\d+)\-(\d+)\-(\w+)', tag)
+    if ma is None:
+        print("The git tag {} does not match the format of v1.y.z-n-abcdef1")
+        exit(1)
+    major, minor, patch, dirty, hash = ma.groups()
+    branch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode()
+    ma = re.match(r'(\d)+\.(\d+)', branch)
+    if ma is not None:
+        branch_major, branch_minor = ma.groups()
+        branch_patch = int(patch) + 1
+    else:
+        branch_major = major
+        branch_minor = int(minor) + 1
+        branch_patch = 0
+        dirty = 1
+    version = "{major}.{minor}.{patch}".format(major=branch_major, minor=branch_minor, patch=branch_patch)
+    if int(dirty) > 0:
+        version = "{version}.dev{dirty}".format(version=version, dirty=dirty)
+    return version
+
+
+version = get_version()
 print("Version:", version)
 
 setup(
